5d010fd91997c3424246213ff4de3e4dc355e995
unknown <anshulgoel48@yahoo.com.au>
2022-03-16 14:52:22 +0530

    initial commit

---
+++ src/main/java/tcs/com/devopsgradle/DevopsGradleApplication.java
@@ -0,0 +1,13 @@
+package tcs.com.devopsgradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsGradleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tcs/com/devopsgradle/DevopsGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package tcs.com.devopsgradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
